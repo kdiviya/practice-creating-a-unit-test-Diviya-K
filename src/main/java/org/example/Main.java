adad6944f81196e6a704e3c1756ff68e9912176d
Kdiviya <eraiswaryabe@gmail.com>
2025-06-03 16:04:39 -0500

    Init Files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
 package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        //create object for the scanner class to get the input from the user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age(less than 100): ");
+
+        //Assign the user entered age to the variable "age"
+        int age = scanner.nextInt();
+        int yearsLeft = calculateYears(age); //calling the method
+        scanner.close();
+        System.out.println(yearsLeft + " years left to reach 100");
+
     }
+
+    //method to calculate years
+    public static int calculateYears(int age) {
+        int years;
+        return years = 100 - age;
+
+    }
+
 }
